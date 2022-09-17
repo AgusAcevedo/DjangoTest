15385d01d7ef520ca0bf7c3b5430c61f01936e12
Agustin Acevedo <agustinacevedo@Agustins-MacBook-Air.local>
2022-09-17 20:20:17 -0300

    Unidad 3 Finalizada

--- diplotst/settings.py
+++ diplotst/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 
     #Mis apps
-    'vistaprevia',
+    'vistaprevia.apps.VistapreviaConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIR = (os.path.join(BASE_DIR,'static_dev'),)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
