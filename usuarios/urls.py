fd5fbc852a09f8f5c08517be968e4eabb6fc4dad
Agustin Acevedo <agustinacevedo@Agustins-MacBook-Air.local>
2022-09-25 03:17:54 -0300

    Final Unidad 4 Funcionando

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from usuarios import views
+from vistaprevia import views
 
 urlpatterns = [
     path('', views.index, name='index'),
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    params = {}
+    params['nombre_sitio'] = 'Libros Online'
+    return render(request,'vistaprevia/index.html', params)
