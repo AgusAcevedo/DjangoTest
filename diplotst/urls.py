17945ff69221fff032a05d4541e8ae03096a9297
Agustin Acevedo <agustinacevedo@Agustins-MacBook-Air.local>
2022-10-08 05:18:29 -0300

    Modulo 2 Unidad 2 Finalizado

--- diplotst/urls.py
+++ diplotst/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('vistaprevia/', include('vistaprevia.urls')),
     path('accounts/', include('registration.backends.default.urls')),
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
-]
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
